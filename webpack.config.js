2625efd46846c58ddb0347421c753529b406c3a6
LukeMcCann <developer-lukemccann@outlook.com>
2022-07-06 19:25:01 +0100

    Add TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,29 @@ const path = require('path')
 const NodemonPlugin = require('nodemon-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.mjs',
+  entry: './src/index.ts',
   mode: 'development',
   target: 'node',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3000
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+        {
+            test: /\.ts?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   plugins: [
     new NodemonPlugin()
